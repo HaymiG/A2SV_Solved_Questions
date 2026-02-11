1a0c37db9b50ae6f6232b7e4d97ccca18fc43c21
haymanot Getachew <154754439+HaymiG@users.noreply.github.com>
2026-02-11 14:39:27 +0300

    Time: 0 ms (100%), Space: 13.2 MB (49.42%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        nums_hash = {}
+        for i in range(len(nums)):   
+            complement = target - nums[i]
+            if complement in nums_hash:
+                return [nums_hash[complement], i]
+            nums_hash[nums[i]] = i

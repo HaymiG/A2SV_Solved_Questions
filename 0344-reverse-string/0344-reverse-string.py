c37a8edc9fe5a35224cede2403f2ef528167f70b
haymanot Getachew <154754439+HaymiG@users.noreply.github.com>
2026-02-12 17:32:59 +0300

    Time: 0 ms (100%), Space: 23.7 MB (16.24%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+      l=0
+      r= len(s)-1
+      while l<r:
+        s[l],s[r]=s[r],s[l]
+        l, r= 1+l,r-1
+    
+        

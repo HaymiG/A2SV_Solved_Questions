0891db1e40e375b247baaf951ddf905acc8024be
haymanot Getachew <haymig75@gmail.com>
2026-03-17 17:46:24 +0300

    Time: 0 ms (100%), Space: 19.5 MB (79.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,15 +1,22 @@
-class Solution(object):
-
-    def myPow(self, x, n):
-        if n == 0: return 1
-        if n == 1: return x
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
         
-        if n < 0:
-            n = -n
-            x = 1/x
+      if n == 0 :
+        return 1
+      if n < 0 :
+        return 1 / self.myPow( x , -n)
 
-        if n%2 == 0:
-            return self.myPow(x*x, n//2)
-        else:
-            return x * self.myPow(x*x, n//2)
+        
+      half =  self.myPow(x , n //2 )
+      if n % 2 == 0 :
+        return half * half
+      else :
+        return x * half * half
+
+          
+     
+        
+
+
+        
         

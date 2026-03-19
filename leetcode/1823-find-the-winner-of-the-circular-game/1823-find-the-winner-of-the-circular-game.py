bd939ccdf517e40fb82652bb7a929cd87b90db3c
haymanot Getachew <haymig75@gmail.com>
2026-03-19 21:57:32 +0300

    Add solution for Find the Winner of the Circular Game

---
+++ leetcode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        winner = 0
+        for i in range(1, n + 1):
+            winner = (k + winner) % i
+            print(winner + 1)  
+        return winner + 1

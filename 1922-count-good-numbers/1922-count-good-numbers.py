df5f71a85783b01663c6d4c05eef4f66c817a0b8
haymanot Getachew <haymig75@gmail.com>
2026-03-31 08:31:46 +0300

    Time: 4 ms (7.38%), Space: 19.3 MB (46.1%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,17 +1,15 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10 ** 9 + 7
-        even_idx = (n+ 1) //2
-        odd_idx = n // 2
-        def pow(x , y):
+        even = (n + 1) // 2
+        odd = n // 2
+        def pow(x, y ):
             if y == 0 :
                 return 1
             if y % 2 == 0 :
-                half = pow(x , y // 2)
+                half = pow(x , y//2)
                 return half * half % MOD
             else :
-                return x * pow(x , y-1) % MOD
-        return (pow(5 , even_idx)*pow(4 , odd_idx)) % MOD
+                return  x* pow(x , y-1) % MOD
 
-
-        
+        return (pow(5 ,even)* pow(4,odd)) % MOD

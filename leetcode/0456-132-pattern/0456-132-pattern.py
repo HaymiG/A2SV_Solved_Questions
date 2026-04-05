2fba220b48c5c3250e18d17a8f83d39b653f8e80
haymanot Getachew <haymig75@gmail.com>
2026-04-05 15:49:35 +0300

    Add solution for 132 Pattern

--- leetcode/0456-132-pattern/0456-132-pattern.py
+++ leetcode/0456-132-pattern/0456-132-pattern.py
@@ -5,18 +5,17 @@ class Solution:
             return False
 
         decreasing_stack = deque()
-        max_third_element = float('-inf')
+        max_third_element = float("-inf")
 
         for i in range(length - 1, -1, -1):
             current_number = nums[i]
 
             if current_number < max_third_element:
-                return True  
+                return True
 
             while decreasing_stack and decreasing_stack[0] < current_number:
                 max_third_element = decreasing_stack.popleft()
 
-            
             decreasing_stack.appendleft(current_number)
 
-        return False  
+        return False

2b52b08e6dca0ad1e37ee0ec1ebbc9c95fe7a507
haymanot Getachew <haymig75@gmail.com>
2026-04-20 12:18:15 +0300

    Add solution for Longest Common Prefix

---
+++ leetcode/0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        
+        new  = strs[0]
+        for i in range(1 , len(strs)):
+            word = strs[i]
+
+            while new and word[:len(new)] != new:
+                new = new[:-1]
+
+        return new 
+            
+
+        
+
+

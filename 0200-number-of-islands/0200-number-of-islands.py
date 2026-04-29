23f7e7af16445a6461b950a58a48686cf9a254f9
haymanot Getachew <haymig75@gmail.com>
2026-04-29 17:55:19 +0300

    Time: 243 ms (61.35%), Space: 21.4 MB (91.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        islands = 0
+        row = len(grid)
+        col = len(grid[0])
+        directions = [(1 , 0) , (-1 , 0 ), (0 , -1) , (0 , 1)]
+        island = 0 
+        
+        def inbound(nr , nc):
+            return  0 <= nr < row and 0 <= nc < col
 
-        def inbound(r, c):
-            return 0 <= r < rows and 0 <= c < cols
 
-        def dfs(r, c):
-            grid[r][c] = '0'  # mark visited
+        def dfs( r , c):
+            
+            grid[r][c] = "0"
 
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
+            for dr , dc in directions:
+                nr = r + dr
+                nc = c + dc
 
-                if inbound(nr, nc) and grid[nr][nc] == '1':
-                    dfs(nr, nc)
+                if inbound(nr, nc) and grid[nr][nc] == "1":
+                    dfs( nr , nc)
+        
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1':
-                    islands += 1
-                    dfs(r, c)
-
-        return islands
+        for r in range(row ):
+            for c in range(col):
+                if grid[r][c] == "1":
+                    dfs(r , c)
+                    island += 1
+        return island

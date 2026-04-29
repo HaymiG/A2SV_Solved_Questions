85989880a0ad9feff54bccdc04a9d8c5702c8008
haymanot Getachew <haymig75@gmail.com>
2026-04-29 17:58:49 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,23 +2,32 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         row = len(grid)
         col = len(grid[0])
-        directions = [(1 , 0) , (-1 , 0 ), (0 , -1) , (0 , 1)]
+        # directions = [(1 , 0) , (-1 , 0 ), (0 , -1) , (0 , 1)]
         island = 0 
         
-        def inbound(nr , nc):
-            return  0 <= nr < row and 0 <= nc < col
+        def inbound(r , c):
+            return  0 <= r < row and 0 <= c < col
 
 
         def dfs( r , c):
+            if not inbound(r , c) or grid[r][c] == "0":
+                return 
             
             grid[r][c] = "0"
 
-            for dr , dc in directions:
-                nr = r + dr
-                nc = c + dc
+            dfs(r - 1 , c)
+            dfs(r + 1 ,c)
+            dfs(r , c- 1 )
+            dfs(r , c + 1)
 
-                if inbound(nr, nc) and grid[nr][nc] == "1":
-                    dfs( nr , nc)
+            
+
+            # for dr , dc in directions:
+            #     nr = r + dr
+            #     nc = c + dc
+
+            #     if inbound(nr, nc) and grid[nr][nc] == "1":
+            #         dfs( nr , nc)
         
 
         for r in range(row ):

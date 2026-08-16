077475a1e243633d232e17681d7b20f3fe4aa6b9
haymanot Getachew <haymig75@gmail.com>
2026-08-16 22:34:11 +0300

    Time: 0 ms (100%), Space: 21.7 MB (10.28%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-    
-
-        nums_count = Counter(nums)
-        for value , count in nums_count.items():
+        counts = Counter(nums)
+        for value , count in counts.items():
             if count == 1:
                 return(value)
-
-        
         
